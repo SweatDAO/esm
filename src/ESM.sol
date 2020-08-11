0addd5f5e0e0b8b879a112cc2ec4f37ed1e5ebb1
stefanionescu <stefanionescu@protonmail.com>
2020-08-11 16:20:12 +0100

    Tests work

--- src/ESM.sol
+++ src/ESM.sol
@@ -72,26 +72,6 @@ contract ESM {
     event Shutdown();
     event FailRecomputeThreshold(bytes revertReason);
 
-    // --- Modifiers ---
-    modifier {
-        _;
-        assembly {
-            // log an 'anonymous' event with a constant 6 words of calldata
-            // and four indexed topics: selector, caller, arg1 and arg2
-            let mark := mload(0x40)                   // end of memory ensures zero
-            mstore(0x40, add(mark, 288))              // update free memory pointer
-            mstore(mark, 0x20)                        // bytes type data offset
-            mstore(add(mark, 0x20), 224)              // bytes size (padded)
-            calldatacopy(add(mark, 0x40), 0, 224)     // bytes payload
-            log4(mark, 288,                           // calldata
-                 shl(224, shr(224, calldataload(0))), // msg.sig
-                 caller(),                            // msg.sender
-                 calldataload(4),                     // arg1
-                 calldataload(36)                     // arg2
-                )
-        }
-    }
-
     constructor(
       address protocolToken_,
       address globalSettlement_,
